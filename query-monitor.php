f78d0a3cf6d2f5667dccc45b6ce5bf09dce3fc45
Nabeel Molham <nmolham@godaddy.com>
2024-02-15 13:31:43 +1100

    init

---
+++ query-monitor.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: PHP Melbourne - Custom WP-CLI Command
+ * Description: Introduction to registering custom WP-CLI commands
+ * Version: 1.0.0
+ * Plugin URI: https://github.com/N-Molham/php-meetup-wp-cli
+ * Author: Nabeel Molham
+ * Author URI: https://nabeel.molham.me/
+ * Requires PHP: 8.1
+ */
+
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+
